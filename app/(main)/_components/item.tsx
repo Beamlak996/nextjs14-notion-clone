da8a47872bc6ba13bb06eff94a3355446ddec413
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-12-29 10:00:07 -0900

    Editor Component Done

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -49,6 +49,7 @@ export const Item = ({
     if(!id) return
 
     const promise = archive({id})
+      .then(()=>router.push(`/documents`))
 
     toast.promise(promise, {
       loading: "Moving new note to trash...",
@@ -73,7 +74,7 @@ export const Item = ({
         if(!expanded) {
           onExpand?.()
         }
-        // router.push(`/documents/${documentId}`);
+        router.push(`/documents/${documentId}`);
       })
       toast.promise(promise, {
         loading: "Creating a new note...",
